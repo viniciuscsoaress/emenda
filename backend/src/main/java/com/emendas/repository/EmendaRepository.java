665eb9d6c758189c8c30c4401ec5c53bf930bdb5
viniciuscsoaress <vinicius.c.soares2@gmail.com>
2026-03-22 19:41:38 -0300

    Add files via upload

---
+++ backend/src/main/java/com/emendas/repository/EmendaRepository.java
@@ -0,0 +1,5 @@
+
+package com.emendas.repository;
+import com.emendas.model.Emenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface EmendaRepository extends JpaRepository<Emenda, Long> {}
