d1326442bece16c4f6cea64e53058bd568432455
viniciuscsoaress <vinicius.c.soares2@gmail.com>
2026-03-22 21:04:50 -0300

    Delete backend/src/main directory

--- backend/src/main/java/com/emendas/repository/EmendaRepository.java
+++
@@ -1,5 +0,0 @@
-
-package com.emendas.repository;
-import com.emendas.model.Emenda;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface EmendaRepository extends JpaRepository<Emenda, Long> {}
